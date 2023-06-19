9b8e4c633b801d925434acbae3e55e8ac2ce83ac
pathik <pathikchauhan17@gmail.com>
2023-06-19 20:22:03 +0530

    s

--- server/src/index.js
+++ server/src/index.js
@@ -2,17 +2,19 @@ import express from "express"
 import mongoose from "mongoose"
 import { userrouter } from "./route/user.js"
 import { recepiesrouter } from "./route/recipies.js"
+import dotenv from "dotenv"
 import cors from "cors"
+
+dotenv.config()
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/", userrouter)
 app.use("/recepies", recepiesrouter)
-mongoose.connect(
-  "mongodb+srv://pathik2003:t8ydab4BSZQyuAUf@cluster0.dn2rrqx.mongodb.net/"
-)
+const connectString = process.env.DATABASE_URL
+mongoose.connect(connectString)
 app.listen(port, () => {
   console.log("server started")
 })
