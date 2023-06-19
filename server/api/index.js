fd42ea8e2d762e6ee0f6b0ca74e4ce24f39949b3
Pathik1712 <128806432+Pathik1712@users.noreply.github.com>
2023-06-19 23:23:11 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,6 @@ import express from "express"
 import mongoose from "mongoose"
 import { userrouter } from "./route/user.js"
 import { recepiesrouter } from "./route/recipies.js"
-import dotenv from "dotenv"
 import cors from "cors"
 
 dotenv.config()
